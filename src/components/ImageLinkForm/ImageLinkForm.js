8eaedd5847bade677676c80683833969c8c135c2
Rajiv <rajeevnayan5055@gmail.com>
2021-07-18 15:41:04 +0530

    update home page

--- src/components/ImageLinkForm/ImageLinkForm.js
+++ src/components/ImageLinkForm/ImageLinkForm.js
@@ -5,7 +5,7 @@ const ImageLinkForm = ({ onInputChange, onButtonSubmit }) => {
   return (
     <div>
       <p className='f3'>
-        {'This Magic Brain will detect faces in your pictures. Git it a try.'}
+        {'Want to try. insert image url'}
       </p>
       <div className='center'>
         <div className='form center pa4 br3 shadow-5'>
