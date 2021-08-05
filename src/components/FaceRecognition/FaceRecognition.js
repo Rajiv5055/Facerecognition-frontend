3d3930e2a28286fce5fb0692f243a882e97fe773
Rajiv <rajeevnayan5055@gmail.com>
2021-08-05 13:34:43 +0530

    updating.

--- src/components/FaceRecognition/FaceRecognition.js
+++ src/components/FaceRecognition/FaceRecognition.js
@@ -1,11 +1,15 @@
 import React from 'react';
 import './FaceRecognition.css';
-
+import Tippy from "@tippyjs/react";
+import "tippy.js/dist/tippy.css";
+import "tippy.js/themes/light.css";
+import "tippy.js/animations/perspective.css";
+import "tippy.js/animations/scale.css";
 const FaceRecognition = ({ imageUrl, box }) => {
   return (
     <div className='center ma'>
       <div className='absolute mt2'>
-        <img id='inputimage' alt='' src={imageUrl} width='500px' heigh='auto'/>
+        <img id='inputimage' alt='' src={imageUrl} width='500px' heigh='auto'/> 
         <div className='bounding-box' style={{top: box.topRow, right: box.rightCol, bottom: box.bottomRow, left: box.leftCol}}></div>
       </div>
     </div>
--- src/components/Profile/Profile.js
+++ src/components/Profile/Profile.js
@@ -2,8 +2,8 @@ import React from 'react';
 import profile from './profile.png';
 const Profile = ({ name, email, entries, joined, onRouteChange }) => {
 	return(
-     <article className="br3 ba b--black-10 mv4 w-100 w-50-m w-25-l mw6 shadow-5 center">
-        <main className="pa4 black-80">
+     <article className="br3 ba b--black-10 mv4 w-200 w-100-m w-50-l mw6 shadow-5 center">
+        <main className="black-80">
           <div className="measure">
             <fieldset id="sign_up" className="ba b--transparent ph0 mh0">
               <img alt="" width="100px" src={profile} />
